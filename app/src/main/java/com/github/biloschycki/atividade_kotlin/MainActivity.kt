6f4ce58c9ea15c4729fa5a57a9e6d645d2b01fa7
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-17 20:29:25 -0300

    subindo atividade

---
+++ app/src/main/java/com/github/biloschycki/atividade_kotlin/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.github.biloschycki.atividade_kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.github.biloschycki.atividade_kotlin.screens.LoginScreen
+import com.github.biloschycki.atividade_kotlin.screens.MenuScreen
+import com.github.biloschycki.atividade_kotlin.screens.PedidosScreen
+import com.github.biloschycki.atividade_kotlin.screens.PerfilScreen
+import com.github.biloschycki.atividade_kotlin.ui.theme.AtividadekotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AtividadekotlinTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                    ) {
+                        composable(route = "login") {
+                            LoginScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "menu") {
+                            MenuScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "pedidos") {
+                            PedidosScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "perfil") {
+                            PerfilScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
